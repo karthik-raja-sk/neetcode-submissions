aa46872cf46cc1a884a9410484829ca7299365e0
KARTHIK RAJA S <saminathankarthikraja@gmail.com>
2026-06-24 16:32:48 +0530

    0162-FIND-PEAK-ELEMENT Stats: Time: 0 ms (100%), Space: 44.2 MB (54.89%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int max=Integer.MIN_VALUE;
+        int ind=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>max){
+                max=nums[i];
+                ind=i;
+            }
+        }
+        return ind;
+    }
+}
